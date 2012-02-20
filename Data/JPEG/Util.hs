21d7e021c41369d00df5ff6c7f71da2961225f65
Myles C. Maxfield <myles.maxfield@gmail.com>
2012-02-19 22:21:01 -0800

    Cleaner implementation of blockOrder

--- Data/JPEG/Util.hs
+++ Data/JPEG/Util.hs
@@ -7,6 +7,7 @@ import Data.Attoparsec
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Vector as V
+import Control.DeepSeq
 import Debug.Trace (trace)
 
 breakWord8 :: Word8 -> (Word8, Word8)
@@ -20,19 +21,26 @@ componentize ([] : _) = []
 componentize l = (map head l) : (componentize $ map tail l)
 
 blockOrder :: Int -> Int -> Int -> [[a]] -> [a]
-blockOrder width_in_clusters cluster_width cluster_height cluster_order = rowed
-  where clusterRows [] = []
-        clusterRows c = (L.take width_in_clusters c) : (clusterRows $ L.drop width_in_clusters c)
-        cluster_rows = clusterRows cluster_order
-        replicated = map (replicate cluster_height) cluster_rows
-        rowed = concat $ map (concat . zipWith (\ i r -> concat $ map ((L.take cluster_width) . (drop (i * cluster_width))) r) [0..]) replicated
+blockOrder width_in_clusters cluster_width cluster_height cluster_order = map f [0 .. cluster_width * cluster_height * length cluster_order - 1]
+  where f index = (cluster_order_v V.! index_cluster) V.! cluster_index
+          where xblock = index `mod` (width_in_clusters * cluster_width)
+                yblock = index `div` (width_in_clusters * cluster_width)
+                xcluster = xblock `div` cluster_width
+                ycluster = yblock `div` cluster_height
+                index_cluster = ycluster * width_in_clusters + xcluster
+                clusterx = xblock `mod` cluster_width
+                clustery = yblock `mod` cluster_height
+                cluster_index = clustery * cluster_width + clusterx
+                
+                
+        cluster_order_v = V.fromList $ map V.fromList cluster_order
 
 indices :: [(Int, Int)]
 indices = (concat $ map (\ x -> map (\ y -> (y, x-y)) (if x `mod` 2 == 0 then [0..x] else [x,x-1..0])) [0..7]) ++
           (concat $ map (\ x -> map (\ y -> (x-y, y)) (if x `mod` 2 == 0 then [7,6..x-7] else [x-7..7])) [8..15])
 
 indices' :: Int -> Int -> Int
-indices' x y = m M.! (x, y)
+indices' x y = m `deepseq` (m M.! (x, y))
   where m = foldl (\ m (v, i) -> M.insert v i m) M.empty $ zip indices [0..]
 
 roundUp :: Integral a => a -> a -> a
